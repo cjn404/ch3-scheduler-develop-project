555ce73b8a44138cbc651d87a27d70e452db1ba4
cjn404 <moi404@naver.com>
2025-08-14 12:59:14 +0900

    [feat/lv4] feat: 일정 작성자명과 유저명 동기화 시 entity 종속성 제거

--- src/main/java/org/example/ch3schedulerdevelopproject/entity/Schedule.java
+++ src/main/java/org/example/ch3schedulerdevelopproject/entity/Schedule.java
@@ -13,7 +13,6 @@ public class Schedule extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String password;
-    private String name;
     private String title;
     private String content;
 
@@ -24,14 +23,12 @@ public class Schedule extends BaseEntity {
     public Schedule(User user, String password, String title, String content) {
         this.user = user;
         this.password = password;
-        this.name = user.getName(); // Schedule 생성 및 수정 시 User name 동기화
         this.title = title;
         this.content = content;
     }
 
     // 일정만 수정하므로 User user 파라미터를 추가 X
     public void updateSchedule(String title, String content) {
-        this.name = user.getName();
         this.title = title;
         this.content = content;
     }
